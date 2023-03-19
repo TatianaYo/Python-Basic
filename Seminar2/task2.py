c8e7a289e9e0db44e1023e473ea57ff854b39a8f
TatianaYo <yoienko@yandex.ru>
2023-03-19 23:00:09 +0300

    seminar 2

--- Seminar2/task2.py
+++ Seminar2/task2.py
@@ -1,10 +1,10 @@
 # Напишите программу, которая принимает две строки вида “a/b” - дробь с числителем и знаменателем. 
 # Программа должна возвращать сумму и произведение* дробей. Для проверки своего кода используйте модуль fractions.
-#import fractions
+import fractions
 
-number_1 = str(input('Введите первую дробь типа a/b: '))
-number_2 = str(input('Введите вторую дробь типа a/b: '))
-print(type(number_1))
+number_1 = fractions.Fraction(input('Введите первую дробь типа a/b: '))
+number_2 = fractions.Fraction(input('Введите вторую дробь типа a/b: '))
+
 sum = number_1 + number_2
 res = number_1 * number_2
 
