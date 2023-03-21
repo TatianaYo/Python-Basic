0fe5da64df19ccfacb9bdd16ca61f67e96c660e4
TatianaYo <yoienko@yandex.ru>
2023-03-22 00:50:45 +0300

    edit in home work seminar 3

--- Home_work_seminar3/HW_task3.py
+++ Home_work_seminar3/HW_task3.py
@@ -1,11 +1,27 @@
-# Дан список повторяющихся элементов. Вернуть список с дублирующимися элементами. В результирующем списке не должно быть дубликатов.
+# В большой текстовой строке подсчитать количество встречаемых слов и вернуть 10 самых частых. Не учитывать знаки препинания
+# и регистр символов. За основу возьмите любую статью из википедии или из документации к языку.
 
-list1 = [1, 1, 1, 5, 12, 7, 2, 5, 6, 12, 5, 1, 7, 7, 9, 12, 1, 5, 7]
-list2 = []
 
-for item in list1:
-    if list1.count(item) > 1:
-        list2.append(item)
-    #if list2.count(item) > 1:
-        #list2.remove(item)
-print(*sorted(set(list2)))
+s = '''Emancipation generally means to free a person from a previous restraint or legal disability.
+    More broadly, it is also used for efforts to procure economic and social rights, political rights or equality, often for a 
+    specifically disenfranchised group, or more generally, in discussion of many matters.
+    Among others, Karl Marx discussed political emancipation in his 1844 essay "On the Jewish Question", 
+    although often in addition to (or in contrast with) the term human emancipation. 
+    Marx's views of political emancipation in this work were summarized by one writer as entailing 
+    "equal status of individual citizens in relation to the state, equality before the law, regardless of religion, property,
+    or other "private" characteristics of individual people."'''
+punct = ',.!?:;"()'
+
+#list1 = [word.rstrip(punct) for word in s.split() if len(word.rstrip(punct)) > 1]
+#print(*sorted(set(list1), key=list1.count, reverse=True)[:10], sep='\n')
+
+
+
+res = len(s.split())
+print('Количество слов = ', + res)
+s = s.split()
+#print(s)
+for word in s:
+    if len(word.rstrip(punct)) > 1:
+        word.rstrip(punct)
+print(*sorted(set(s), key=s.count, reverse=True)[:10], sep='\n')
--- Home_work_seminar3/HW_task4.py
+++ Home_work_seminar3/HW_task4.py
@@ -1,27 +1,15 @@
-# В большой текстовой строке подсчитать количество встречаемых слов и вернуть 10 самых частых. Не учитывать знаки препинания
-# и регистр символов. За основу возьмите любую статью из википедии или из документации к языку.
+# Создайте словарь со списком вещей для похода в качестве ключа и их массой в качестве значения. Определите какие
+# вещи влезут в рюкзак передав его максимальную грузоподъёмность. Достаточно вернуть один допустимый вариант.
 
+dict1 = {'cup': 0.5, 'tea': 0.4, 'clother': 2, 'tent': 4, 'sleeping_bag': 3, 'matches': 0.02}
 
-s = '''Emancipation generally means to free a person from a previous restraint or legal disability.
-    More broadly, it is also used for efforts to procure economic and social rights, political rights or equality, often for a 
-    specifically disenfranchised group, or more generally, in discussion of many matters.
-    Among others, Karl Marx discussed political emancipation in his 1844 essay "On the Jewish Question", 
-    although often in addition to (or in contrast with) the term human emancipation. 
-    Marx's views of political emancipation in this work were summarized by one writer as entailing 
-    "equal status of individual citizens in relation to the state, equality before the law, regardless of religion, property,
-    or other "private" characteristics of individual people."'''
-punct = ',.!?:;"()'
+back = 5
 
-#list1 = [word.rstrip(punct) for word in s.split() if len(word.rstrip(punct)) > 1]
-#print(*sorted(set(list1), key=list1.count, reverse=True)[:10], sep='\n')
+for key, item in dict1.items():
+    if item <= back:
+        back = back - item
+        print(key)
+    else:
+        continue
 
-
-
-res = len(s.split())
-print('Количество слов = ', + res)
-s = s.split()
-#print(s)
-for word in s:
-    if len(word.rstrip(punct)) > 1:
-        word.rstrip(punct)
-print(*sorted(set(s), key=s.count, reverse=True)[:10], sep='\n')
+    
